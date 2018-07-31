d9b3d1713431fe25be7d409ae9e2fbe42c81c5b3
Ryan Heap <rheap@degdigital.com>
2018-07-31 12:36:38 -0500

    added colors and formatting to console logger and output task duration

--- tests/runTask.test.js
+++ tests/runTask.test.js
@@ -1,4 +1,5 @@
 const skeletor = require('../index');
+const mockNow = require('jest-mock-now');
 
 const validConfig = {
 	tasks: [
@@ -43,11 +44,12 @@ const validConfigWithPlugin = {
 
 const validResponse = {
 	taskName: 'task1',
-    status: 'complete',
+	status: 'complete',
+	duration: 0,
     subTasks: [ 
-    	{ taskName: 'subTask1', status: 'complete', plugins: [] },
-        { taskName: 'subTask2', status: 'complete', plugins: [] },
-        { taskName: 'subTask3', status: 'complete', plugins: [] } 
+    	{ taskName: 'subTask1', status: 'complete', plugins: [], duration: 0 },
+        { taskName: 'subTask2', status: 'complete', plugins: [], duration: 0 },
+        { taskName: 'subTask3', status: 'complete', plugins: [], duration: 0 } 
     ]
 };
 
@@ -71,68 +73,73 @@ test('A custom logger is used by runTask()', () => {
 		.catch(e => expect(logger.error.mock.calls.length).toBe(1));
 });
 
-test('runTask() returns an error if no config is specified', () => {
-	expect.assertions(1);
-  	return skeletor().runTask('build')
-  		.catch(e => expect(e).toMatch("Configuration Error -- Error: ENOENT: no such file or directory, access './skeletor.config.js'"));
-});
-
-test('runTask() returns an error if task does not exist in config', () => {
-	const skel = skeletor();
-	skel.setConfig({tasks: [{name: 'task1'}]});
-
-	expect.assertions(1);
-	return skel.runTask('task2')
-		.catch(e => expect(e).toMatch('ERROR: Could not find task "task2"'));
-});
-
-test('runTask() runs specified task', () => {
-	const skel = skeletor();
-	skel.setConfig(validConfig);
-
-	expect.assertions(1);
-	return skel.runTask('task1')
-		.then(response => expect(response).toEqual(validResponse));
-});
-
-test('runTask() runs only specified subtasks', () => {
-	const skel = skeletor();
-	const options = { subTasksToInclude: ['subTask1', 'subTask2'] };
-	const expectedResponse = {
-		taskName: 'task1',
-	    status: 'complete',
-	    subTasks: [ 
-	    	{ taskName: 'subTask1', status: 'complete', plugins: [] },
-	        { taskName: 'subTask2', status: 'complete', plugins: [] }
-	    ]
-	};
-
-	skel.setConfig(validConfig);
-
-	expect.assertions(1);
-	return skel.runTask('task1', options)
-		.then(response => expect(response).toEqual(expectedResponse));
-});
-
-test('runTask() runs plugin', () => {
-	const skeletorPlugin = require('skeletorPlugin')();
-	const runSpy = jest.spyOn(skeletorPlugin, 'run');
-
-	const skel = skeletor();
-	skel.setConfig(validConfigWithPlugin);
-
-	const pluginOptions = {
-		logger: {
-			info: jest.fn()
-		}
-	};
+describe('runTask() returns an error', () => {
+	test('if no config is specified', () => {
+		expect.assertions(1);
+		  return skeletor().runTask('build')
+			  .catch(e => expect(e).toMatch("Configuration Error -- Error: ENOENT: no such file or directory, access './skeletor.config.js'"));
+	});
 	
-	expect.assertions(2);
-	return skel.runTask('task1', pluginOptions)
-		.then(response => {
-			expect(runSpy.mock.calls.length).toEqual(1);
-			expect(runSpy.mock.calls[0]).toEqual([skeletorPluginConfig.config, pluginOptions, skel]);		
-		});
+	test('if task does not exist in config', () => {
+		const skel = skeletor();
+		skel.setConfig({tasks: [{name: 'task1'}]});
+	
+		expect.assertions(1);
+		return skel.runTask('task2')
+			.catch(e => expect(e).toMatch('ERROR: Could not find task "task2"'));
+	});
+});
+
+describe('runTask() runs', () => {
+	beforeEach(() => {
+		mockNow(new Date('2018-02-17'));
+	});
+
+	test('specified task', () => {
+		const skel = skeletor();
+		skel.setConfig(validConfig);
+		expect.assertions(1);
+		return skel.runTask('task1')
+			.then(response => expect(response).toEqual(validResponse));
+	});
+	
+	test('only specified subtasks', () => {
+		const skel = skeletor();
+		const options = { subTasksToInclude: ['subTask1', 'subTask2'] };
+		const expectedResponse = {
+			taskName: 'task1',
+			status: 'complete',
+			duration: 0,
+			subTasks: [ 
+				{ taskName: 'subTask1', status: 'complete', plugins: [], duration: 0 },
+				{ taskName: 'subTask2', status: 'complete', plugins: [], duration: 0 }
+			]
+		};
+	
+		skel.setConfig(validConfig);
+	
+		expect.assertions(1);
+		return skel.runTask('task1', options)
+			.then(response => expect(response).toEqual(expectedResponse));
+	});
+	
+	test('plugin', () => {
+		const skeletorPlugin = require('skeletorPlugin')();
+		const runSpy = jest.spyOn(skeletorPlugin, 'run');
+	
+		const skel = skeletor();
+		skel.setConfig(validConfigWithPlugin);
+	
+		const pluginOptions = {};
+		
+		expect.assertions(2);
+		return skel.runTask('task1', pluginOptions)
+			.then(response => {
+				expect(runSpy.mock.calls.length).toEqual(1);
+				expect(runSpy.mock.calls[0]).toEqual([skeletorPluginConfig.config, pluginOptions, skel]);		
+			});
+	});
+
 });
 
 describe('runTask() sets the node environment variable', () => {
